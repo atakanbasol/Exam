3171e28a252ec5259c03811a53389b47f8bd0fd7
atakanbasol <basolatakan8gmail.com>
2023-05-03 23:32:55 +0300

    The exam application I create for distance education

---
+++ src/main/java/com/atakanbasol/exam/ExamApplication.java
@@ -0,0 +1,30 @@
+package com.atakanbasol;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import java.util.TimeZone;
+
+//SECURITY EXCLUDE
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        //org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        //org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+//@SpringBootApplication
+public class ExamApplication {
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+
+    public static void main(String[] args) {
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless", "false");
+        SpringApplication.run(ExamApplication.class, args);
+    }
+
+}
